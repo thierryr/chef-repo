20fa3e83f7acd9a7df752327014d1fffa05f05f6
Jack C Hong <jack.hong@nicta.com.au>
2014-09-05 15:06:21 +1000

    Install OMF RC

--- cookbooks/omf/recipes/default.rb
+++ cookbooks/omf/recipes/default.rb
@@ -6,3 +6,20 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+%w(ruby ruby-dev build-essential libssl-dev).each do |p|
+  package p do
+    action :upgrade
+  end
+end
+
+gem_package "omf_rc" do
+  action :upgrade
+end
+
+bash "install_omf_rc_start_script" do
+  code "install_omf_rc -i"
+end
+
+service "omf_rc" do
+  action [:start, :enable]
+end
