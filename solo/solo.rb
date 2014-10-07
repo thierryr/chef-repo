855eadd2ce4a38972848a30a7081f8cfaa0c98bf
Jack C Hong <jack.hong@nicta.com.au>
2014-10-07 17:07:27 +1100

    Disable EC2 Ohai plugin since it causes problem under XEN

--- solo/solo.rb
+++ solo/solo.rb
@@ -1,3 +1,4 @@
 ssl_verify_mode :verify_peer
 file_cache_path "#{File.expand_path(File.dirname(__FILE__))}/../solo/cache"
 cookbook_path "#{File.expand_path(File.dirname(__FILE__))}/../cookbooks"
+Ohai::Config[:disabled_plugins] = [:EC2]
