b2391bafb00d812722bc7e055b6da7112f324845
thierry <thierry.rakotoarivelo@nicta.com.au>
2014-10-17 11:31:15 +1100

    Modified to start 12 Node Proxy per RC

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ if ! type "chef-solo" > /dev/null; then
   wget https://www.opscode.com/chef/install.sh -O getchef.sh && bash getchef.sh && rm getchef.sh
 fi
 
-wget https://github.com/jackhong/chef-repo/archive/master.tar.gz --no-check-certificate -O chef-repo.tar.gz
+#wget https://github.com/jackhong/chef-repo/archive/master.tar.gz --no-check-certificate -O chef-repo.tar.gz
+wget https://raw.githubusercontent.com/thierryr/chef-repo/master/master.tar.gz --no-check-certificate -O chef-repo.tar.gz
 
 tar xzf chef-repo.tar.gz
 rm -r chef-repo.tar.gz chef-repo 2>/dev/null
