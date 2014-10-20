8b2d52eb3c8c6a39d9f20ecdeb003245d14dc7a6
thierry <thierry.rakotoarivelo@nicta.com.au>
2014-10-21 02:13:01 +1100

    Merged with master at mytestbed.net

--- cookbooks/omf/attributes/default.rb
+++ cookbooks/omf/attributes/default.rb
@@ -8,16 +8,19 @@ default['omf']['uids'] = a.map { |i| b.map { |j| j+i+c } }.flatten
 default['omf']['uid'] =
   begin
     node_json = JSON.parse(File.read("#{File.expand_path(File.dirname(__FILE__))}/../../../node.json"))
-    node_json.values.first["omf_id"]
+    #node_json.values.first["omf_id"]
+    node_json["omf_id"]
   rescue
     Socket.gethostname
   end
 
-default['omf']['oml_collect_uri'] = "tcp:srv.mytestbed.net:3004"
+#default['omf']['oml_collect_uri'] = "tcp:srv.mytestbed.net:3004"
+default['omf']['oml_collect_uri'] = ENV["OML_COLLECT_URI"] && !ENV["OML_COLLECT_URI"].empty? ? ENV["OML_COLLECT_URI"] : "tcp:srv.mytestbed.net:3004"
 
 default['omf']['edges'] =
   begin
-    slice_json = JSON.parse(File.read("#{File.expand_path(File.dirname(__FILE__))}/../../../slice.json"))
+    #slice_json = JSON.parse(File.read("#{File.expand_path(File.dirname(__FILE__))}/../../../slice.json"))
+    slice_json = JSON.parse(File.read("#{File.expand_path(File.dirname(__FILE__))}/../../../../../slice.json"))
     slice_json.values.map do |n|
       n["interfaces"] && n["interfaces"].values.map do |i|
         i["ip"] && i["ip"].map { |ip| ip["address"] }
